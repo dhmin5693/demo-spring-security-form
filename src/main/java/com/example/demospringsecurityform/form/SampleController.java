8ed031273cdc2c2055294871f8d4c1b0394997c3
dhmin5693 <dhmin5693@naver.com>
2021-12-22 00:28:34 +0900

    default setting

---
+++ src/main/java/com/example/demospringsecurityform/form/SampleController.java
@@ -0,0 +1,42 @@
+package com.example.demospringsecurityform.form;
+
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    private static final String MODEL_NAME = "message";
+
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        if (principal == null) {
+            model.addAttribute(MODEL_NAME, "Hello, Spring security!");
+        } else {
+            model.addAttribute(MODEL_NAME, "Hello, " + principal.getName());
+        }
+        
+        
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute(MODEL_NAME, "This is admin, " + principal.getName());
+        return "admin";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute(MODEL_NAME, "This is dashboard, " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model, Principal principal) {
+        model.addAttribute(MODEL_NAME, "This is info, " + principal.getName());
+        return "info";
+    }
+}
