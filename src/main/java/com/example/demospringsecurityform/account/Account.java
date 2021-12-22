764f0f12cd68b2c4d202f757f8de55b778f90ab0
dhmin5693 <dhmin5693@naver.com>
2021-12-23 01:20:01 +0900

    add JPA dependencies and `Account` Entity

---
+++ src/main/java/com/example/demospringsecurityform/account/Account.java
@@ -0,0 +1,34 @@
+package com.example.demospringsecurityform.account;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    @Builder
+    public Account(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
