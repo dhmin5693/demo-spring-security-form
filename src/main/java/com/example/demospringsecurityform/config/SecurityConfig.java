6851320770b23033768d842403dc491c94449ee0
dhmin5693 <dhmin5693@naver.com>
2021-12-30 01:59:33 +0900

    add new security filter

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -1,24 +1,27 @@
 package com.example.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // 설정이 필요한 메소드를 Override하는 식으로 구현
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-                    .mvcMatchers("/", "/info", "/account/**").permitAll()
-                    .mvcMatchers("/admin").hasRole("ADMIN")
-                    .anyRequest().authenticated();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .mvcMatchers("/", "info", "/account/**").permitAll()
+            .mvcMatchers("admin").hasRole("ADMIN")
+            .anyRequest().authenticated();
 
         // security에서 제공하는 기본 form login 기능
-        httpSecurity.formLogin();
-        httpSecurity.httpBasic();
+        http.formLogin();
+        http.httpBasic();
     }
 }
