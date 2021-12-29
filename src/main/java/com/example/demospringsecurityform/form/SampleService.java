a672c2f22366e4f8788503de2460aff358c635d6
dhmin5693 <dhmin5693@naver.com>
2021-12-30 01:07:04 +0900

    get Authentication from Authentication manager

--- src/main/java/com/example/demospringsecurityform/form/SampleService.java
+++ src/main/java/com/example/demospringsecurityform/form/SampleService.java
@@ -1,9 +1,8 @@
 package com.example.demospringsecurityform.form;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +10,9 @@ public class SampleService {
 
     public void dashboard() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        List<?> authorities = authentication.getAuthorities()
-                                            .stream()
-                                            .collect(Collectors.toList());
-        Object credentials = authentication.getCredentials();
-        boolean authenticated = authentication.isAuthenticated();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("=================");
+        System.out.println(authentication);
+        System.out.println(userDetails.getUsername());
     }
 }
