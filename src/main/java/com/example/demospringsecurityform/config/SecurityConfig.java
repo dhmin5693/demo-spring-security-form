89d1167f1fba27a9a761eabac5e663c39c7240e4
dhmin5693 <dhmin5693@naver.com>
2022-01-08 02:46:55 +0900

    add access-denied log

--- src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
@@ -59,7 +60,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 
         http.exceptionHandling()
-            .accessDeniedPage("/access-denied");
+            .accessDeniedHandler((request, response, accessDeniedException) -> {
+                UserDetails principal = (UserDetails) SecurityContextHolder.getContext()
+                                                                           .getAuthentication()
+                                                                           .getPrincipal();
+                String username = principal.getUsername();
+                System.out.println(username + " is denied to access " + request.getRequestURI());
+                response.sendRedirect("/access-denied");
+            });
     }
 
 //    @Override
